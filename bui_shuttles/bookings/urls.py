f8fbdf643dd226cae74fb9b1091d65d167a2f275
Efosa <efosacharlesabu@gmail.com>
2025-05-27 03:43:09 +0100

    chore: took out redundant urls

--- bui_shuttles/bookings/urls.py
+++ bui_shuttles/bookings/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r"bookings", views.Bookings, basename="bookings")
+router.register("", views.Bookings, basename="bookings")
 
 app_name = "bookings"
 urlpatterns = router.urls
--- bui_shuttles/trips/urls.py
+++ bui_shuttles/trips/urls.py
@@ -5,11 +5,15 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register("routes", views.Route, basename="route")
-router.register("trips", views.Trip, basename="trip")
+router.register("", views.Trip, basename="trip")
 
 app_name = "trips"
 urlpatterns = [
-    path("driver/<int:driver_id>/routes/", views.DriverRoutes.as_view(), name="driver_routes"),
+    path(
+        "driver/<int:driver_id>/routes/",
+        views.DriverRoutes.as_view(),
+        name="driver_routes",
+    ),
 ]
 
 urlpatterns += router.urls
